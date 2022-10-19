f5a44caf3928b5a3f01190f331394e58efd780ba
Luisa <luisaquintian@hotmail.com>
2022-10-19 10:40:38 -0300

    Add sidebar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { Header } from "./components/Header";
-
 import "./global.css"
+import styles from "./App.module.css"
+
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
+    <>
     <Header />
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <h1>posts</h1>
+      </main>
+    </div>
+    </>
   )
 }
